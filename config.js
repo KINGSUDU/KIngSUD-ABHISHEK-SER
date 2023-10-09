ed654dcaa1993e590dcb448dc9ce764563c69211
KINGSUDU <145249551+KINGSUDU@users.noreply.github.com>
2023-10-09 19:37:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
+  ['94785214202', '𝑆𝑈𝐷𝛯𝛯𝛲𝑇𝛨𝛥', true],
   [''],
   [''] 
 ] //Number of owners
 
-global.mods = ['919074692450','919074692450'] 
-global.prems = ['919074692450', '919074692450', '919074692450']
+global.mods = ['94785214202','94778599751'] 
+global.prems = ['94785214202', '94778599751']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,16 +47,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
+global.botname = '𝕂𝕚𝕟𝕘♚ S U D U'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️' 
-global.author = 'ABHISHEK SURESH' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
-global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
+global.packname = 'king-sudu🎯bot☘️' 
+global.author = 'King SUDU' 
+global.igfg = '▢ Follow on tiktok\nhttps://www.tiktok.com/@kingsudu99?_t=8fiEI5KmbeT&_r=1\n' 
+global.dygp = 'https://chat.whatsapp.com/EbVGJRPOM5SCpBhhsX6lvX'
 global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
-global.fgyt = 'https://youtube.com/@comedymelodych8468'
-global.fgpyp = 'https://youtube.com/@comedymelodych8468'
-global.fglog = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png' 
+global.fgyt = ''
+global.fgpyp = ''
+global.fglog = 'https://telegra.ph/file/af23b6e1aefa315591996.jpg' 
 
 //imagen
 global.imagen1 = fs.readFileSync('./src/Abhi.jpg')
